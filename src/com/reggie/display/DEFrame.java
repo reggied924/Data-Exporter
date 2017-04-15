d5573820fbf8fe89cc287dcb1b00753bfa79fd17
Reginald Daniel <reginalddaniel924@gmail.com>
2017-04-15 11:25:51 -0400

    Exit menu option operational

--- src/com/reggie/display/DEFrame.java
+++ src/com/reggie/display/DEFrame.java
@@ -1,6 +1,7 @@
 package com.reggie.display;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Created by reginalddaniel on 4/15/17.
@@ -9,7 +10,11 @@ import javax.swing.*;
 public class DEFrame extends JFrame {
 
     JMenuBar mb;
+
     JMenu file;
+    JMenuItem exportData;
+    JMenuItem createConnection;
+    JMenuItem exit;
 
     public DEFrame(){
 
@@ -29,11 +34,40 @@ public class DEFrame extends JFrame {
 
 
         file = new JMenu("File");
+        exportData = new JMenuItem("Export Data");
+        createConnection = new JMenuItem("Create Data Connection");
+        exit = new JMenuItem("Exit");
+        exit.addActionListener(new ExitAction());
+
+        file.add(exportData);
+        file.add(createConnection);
+        file.addSeparator();
+        file.add(exit);
+
 
         mb.add(file);
 
         return mb;
 
+    }
+
+    private class ExitAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            int selection = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?",  "Exit Data Exporter", JOptionPane.YES_NO_OPTION);
+
+            if(selection == JOptionPane.YES_OPTION){
+                System.exit(0);
+            }
+            else{
+
+                //Do nothing
+
+            }
+
+        }
     }
 
 

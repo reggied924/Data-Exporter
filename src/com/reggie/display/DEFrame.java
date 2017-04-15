eadbf8dd0b815888f996031594fd39958efd453b
Reginald Daniel <reginalddaniel924@gmail.com>
2017-04-15 10:59:12 -0400

    Added menu bar

--- src/com/reggie/display/DEFrame.java
+++ src/com/reggie/display/DEFrame.java
@@ -5,7 +5,36 @@ import javax.swing.*;
 /**
  * Created by reginalddaniel on 4/15/17.
  */
+
 public class DEFrame extends JFrame {
+
+    JMenuBar mb;
+    JMenu file;
+
+    public DEFrame(){
+
+        this.setTitle("Data Exporter");
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setJMenuBar(generateJMenuBar());
+        this.setVisible(true);
+
+    }
+
+
+    private JMenuBar generateJMenuBar(){
+
+        mb = new JMenuBar();
+
+
+        file = new JMenu("File");
+
+        mb.add(file);
+
+        return mb;
+
+    }
 
 
 
---
+++ src/com/reggie/main/AppStart.java
@@ -0,0 +1,15 @@
+package com.reggie.main;
+
+import com.reggie.display.DEFrame;
+
+/**
+ * Created by reginalddaniel on 4/15/17.
+ */
+public class AppStart {
+
+    public static void main(String[] args){
+
+        DEFrame frame = new DEFrame();
+
+    }
+}
